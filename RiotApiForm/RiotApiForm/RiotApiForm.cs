ee0a966052f15bebd1fd046fc72434dfff00e5c3
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-12-08 20:18:40 -0500

    Push 1-1
     First working push with properties and packages folder still in.

---
+++ RiotApiForm/RiotApiForm/RiotApiForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MingweiSamuel.Camille.Enums;
+
+
+namespace RiotApiForm
+{
+    static class RiotApiForm
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
